81d6b1fdb4f2520b445d38006f0d92f46cdca034
PacificState <macmo920@gmail.com>
2015-07-07 02:09:31 -0600

    Update AutoRanker.java

--- AutoRanker.java
+++ AutoRanker.java
@@ -1,15 +1,19 @@
 package com.ressamp.autoranker;
+import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import com.ressamp.autoranker.commands.Commands;
 
 public class AutoRanker extends JavaPlugin {
+
 	public void onEnable(){
 		getLogger().info(Color.RED + "AutoRanker Enabled!");
 	}
 	
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+	/* public boolean onCommand2(CommandSender sender, Command command, String label, String[] args) {
 		String cmd = command.getName();
 		if (cmd.equalsIgnoreCase("ar")) {
 			if (args.length !=1) return false;
@@ -23,7 +27,41 @@ public class AutoRanker extends JavaPlugin {
 				return true;
 			}
 		}
+		return false;
+	} */
 	
-	return false;
-	}
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        PlayerEntry player = m_playerManager.getPlayer((sender instanceof Player) ? (Player) sender : null);
+
+        if (!command.getName().equalsIgnoreCase(Commands.COMMAND_MAIN)) {
+            return false;
+        }
+
+        String name = (args != null && args.length > 0) ? args[0] : "";
+
+        if (name.equalsIgnoreCase(Commands.COMMAND_RELOAD)) {
+            doReloadConfig(player, args != null && args.length > 1 ? args[1] : "");
+            return true;
+        } else if (name.equalsIgnoreCase(Commands.COMMAND_HELP)) {
+            String arg = args != null && args.length > 1 ? args[1] : null;
+            return Help.ShowHelp(player, arg);
+        }
+        /*
+        
+        } else if (name.equalsIgnoreCase(Commands.COMMAND_PURGE)) {
+            doPurge(player, args);
+            return true;
+        } else if (name.equalsIgnoreCase(Commands.COMMAND_JOBS)) {
+            doJobs(player, args);
+            return true;
+        } else if (name.equalsIgnoreCase(Commands.COMMAND_CANCEL)) {
+            doCancel(player, args);
+            return true;
+        } else if (name.equalsIgnoreCase(Commands.COMMAND_TOGGLE)) {
+            doToggle(player, args);
+            return true;
+        } */
+
+        return Help.ShowHelp(player, null);
+    }
 }
