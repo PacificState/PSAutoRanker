8d6443c7808c3c1dbd991fc38ed4bfac92362b0e
PacificState <macmo920@gmail.com>
2015-07-07 14:44:40 -0600

    Delete Commands.java

--- Commands.java
+++
@@ -1,8 +0,0 @@
-package com.ressamp.autoranker.commands;
-
-
-public final class Commands {
-    public final static String COMMAND_MAIN = "ar";
-    public final static String COMMAND_RELOAD = "Reload";
-    public final static String COMMAND_HELP = "Help";
-}
