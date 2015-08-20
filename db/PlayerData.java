00f6057dedcf9c0ada05802d25adfb11c81990f1
PacificState <macmo920@gmail.com>
2015-08-20 03:08:34 -0600

    Update PlayerData.java

--- db/PlayerData.java
+++ db/PlayerData.java
@@ -4,4 +4,9 @@ package com.ressamp.autoranker.db;
 
 public class PlayerData {
 
+// Features
+// Checking if the player has a file
+// Creating the file if the player does not
+// Updating the player file
+
 }
